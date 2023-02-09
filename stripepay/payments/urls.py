f46816dda570b4a8511f22f7c7c5621616a33110
SkyFlyer2 <textgame-vlad@yandex.ru>
2023-02-09 23:37:58 +0300

    add some endpoints

--- stripepay/payments/urls.py
+++ stripepay/payments/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'payments'
+
 urlpatterns = [
     path('', views.MainPage.as_view(), name='main'),
+    path('buy/<int:id>/', views.item_purchase, name='item_purchase'),
+    path('item/<int:id>/', views.item_detail, name='item_detail'),
 ]
--- stripepay/payments/views.py
+++ stripepay/payments/views.py
@@ -1,6 +1,14 @@
 from django.views.generic.base import TemplateView
+from django.shortcuts import redirect, render, get_object_or_404
+
 
 class MainPage(TemplateView):
     template_name = 'index.html'
 
 
+def item_detail(request, id):
+    template_name = 'index.html'
+
+
+def item_purchase(request, id):
+    template_name = 'index.html'
