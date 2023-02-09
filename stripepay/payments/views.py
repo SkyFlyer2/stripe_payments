097d3dee9445a05c92f77c11bc80495af40659ef
SkyFlyer2 <textgame-vlad@yandex.ru>
2023-02-10 00:20:18 +0300

    add simple template

--- stripepay/payments/views.py
+++ stripepay/payments/views.py
@@ -1,13 +1,20 @@
 from django.views.generic.base import TemplateView
 from django.shortcuts import redirect, render, get_object_or_404
 
+from .models import Item
+
 
 class MainPage(TemplateView):
     template_name = 'index.html'
 
 
 def item_detail(request, id):
-    template_name = 'index.html'
+    item = get_object_or_404(Item, pk= id)
+    return render(
+        request,
+        'item_detail.html',
+        {'buy_item': item,}
+    )
 
 
 def item_purchase(request, id):
