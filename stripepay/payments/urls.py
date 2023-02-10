d5f56b857ffdc11237fedaf171dab17eb9af143f
Владислав Ковалёв <vladislav.kovalev@autopolygon.base>
2023-02-10 15:47:38 +0300

    checkout

--- stripepay/payments/urls.py
+++ stripepay/payments/urls.py
@@ -5,10 +5,9 @@ from . import views
 app_name = 'payments'
 
 urlpatterns = [
-    path('', views.MainPage.as_view(), name='main'),
+    path('', views.main_page.as_view(), name='main'),
     path('buy/<int:id>/', views.item_purchase, name='item_purchase'),
     path('item/<int:id>/', views.item_detail, name='item_detail'),
-    path('success/', views.SuccessView.as_view()),
-    path('cancelled/', views.CancelledView.as_view()),
-#    path('create-checkout-session/', views.create_checkout_session),
+    path('success/', views.order_success.as_view()),
+    path('cancelled/', views.order_cancelled.as_view()),
 ]
